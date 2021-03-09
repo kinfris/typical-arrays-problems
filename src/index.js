a2353eb804828cb2ec3b5cdcdbd069e425ab7f0a
kinfris <jormungand1616@gmail.com>
2021-03-09 12:51:41 +0300

    feat: finish tap task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } else{
+  let minA = array[0];
+  for(i = 1; i <= array.length; i++){
+    if(minA > array[i]){
+      minA = array[i];
+    }
+  }
+  return minA;
+  }
+
+
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } else{
+  let maxA = array[0];
+  for(i = 1; i <= array.length; i++) {
+    if(maxA < array[i]){
+      maxA = array[i];
+    }
+  }
+  return maxA;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } else{
+  var sum = 0;
+    for(var i = 0; i < array.length; i++) {
+    sum += array[i];
+    }
+  return  sum / array.length;
+  }
 }
